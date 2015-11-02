ed9db3b494b042d442fec423d1ee1a260ef797bd
Juan <juan@juan-ubuntu.(none)>
2015-11-02 20:28:46 -0300

    Sin, Chirp and Voz, .m and datasets updated

--- enfMp3SinMain.m
+++ enfMp3SinMain.m
@@ -5,27 +5,16 @@ addpath ./functions/
 %% Files reading
 
 [senal_20dB, fs]=wavread('./dataset/sin/senal_20dB.wav');
+senal_25dB=wavread('./dataset/sin/senal_25dB.wav');
 senal_30dB=wavread('./dataset/sin/senal_30dB.wav');
+senal_35dB=wavread('./dataset/sin/senal_35dB.wav');
 senal_40dB=wavread('./dataset/sin/senal_40dB.wav');
-senal_50dB=wavread('./dataset/sin/senal_50dB.wav');
-senal_60dB=wavread('./dataset/sin/senal_60dB.wav');
 
-% senal_20dB_mp3=mp3read('./dataset/sin/64_hq/senal_20dB.mp3');
-% senal_30dB_mp3=mp3read('./dataset/sin/64_hq/senal_30dB.mp3');
-% senal_40dB_mp3=mp3read('./dataset/sin/64_hq/senal_40dB.mp3');
-% senal_50dB_mp3=mp3read('./dataset/sin/64_hq/senal_50dB.mp3');
-% % senal_40dB_mp3=mp3read('./dataset/sin/128_hq/senal_40dB.mp3');
-% % senal_50dB_mp3=mp3read('./dataset/sin/128_hq/senal_50dB.mp3');
-% % senal_40dB_mp3=mp3read('./dataset/sin/acid_128_lq/senal_40dB.mp3');
-% % senal_50dB_mp3=mp3read('./dataset/sin/acid_128_lq/50dB.mp3');
-% % senal_50dB_mp3=mp3read('./dataset/sin/acid_128_hq/senal_50dB.mp3');
-% senal_60dB_mp3=mp3read('./dataset/sin/64_hq/senal_60dB.mp3');
-
-senal_20dB_mp3=mp3read('./dataset/sin/256_hq/senal_20dB.mp3');
-senal_30dB_mp3=mp3read('./dataset/sin/256_hq/senal_30dB.mp3');
-senal_40dB_mp3=mp3read('./dataset/sin/256_hq/senal_40dB.mp3');
-senal_50dB_mp3=mp3read('./dataset/sin/256_hq/senal_50dB.mp3');
-senal_60dB_mp3=mp3read('./dataset/sin/256_hq/senal_60dB.mp3');
+senal_20dB_mp3=mp3read('./dataset/sin/senal_20dB.mp3');
+senal_25dB_mp3=mp3read('./dataset/sin/senal_25dB.mp3');
+senal_30dB_mp3=mp3read('./dataset/sin/senal_30dB.mp3');
+senal_35dB_mp3=mp3read('./dataset/sin/senal_35dB.mp3');
+senal_40dB_mp3=mp3read('./dataset/sin/senal_40dB.mp3');
 
 
 %% FFT
@@ -33,19 +22,19 @@ L=length(senal_20dB);
 NFFT = 2^nextpow2(L); % Next power of 2 from length of y
 
 SENAL_20dB = fft(hanning(L).*senal_20dB,NFFT)/L;
-SENAL_30dB = fft(hanning(L).*senal_30dB,NFFT)/L;
-SENAL_40dB = fft(hanning(L).*senal_40dB,NFFT)/L;
-SENAL_50dB = fft(hanning(L).*senal_50dB,NFFT)/L;
-SENAL_60dB = fft(hanning(L).*senal_60dB,NFFT)/L;
+SENAL_30dB = fft(hanning(L).*senal_25dB,NFFT)/L;
+SENAL_40dB = fft(hanning(L).*senal_30dB,NFFT)/L;
+SENAL_50dB = fft(hanning(L).*senal_35dB,NFFT)/L;
+SENAL_60dB = fft(hanning(L).*senal_40dB,NFFT)/L;
 
 % L_mp3=length(senal_50dB_mp3);
 % NFFT_mp3 = 2^nextpow2(L_mp3);
 
 SENAL_20dB_mp3 = fft(hanning(L).*senal_20dB_mp3,NFFT)/L;
-SENAL_30dB_mp3 = fft(hanning(L).*senal_30dB_mp3,NFFT)/L;
-SENAL_40dB_mp3 = fft(hanning(L).*senal_40dB_mp3,NFFT)/L;
-SENAL_50dB_mp3 = fft(hanning(L).*senal_50dB_mp3,NFFT)/L;
-SENAL_60dB_mp3 = fft(hanning(L).*senal_60dB_mp3,NFFT)/L;
+SENAL_30dB_mp3 = fft(hanning(L).*senal_25dB_mp3,NFFT)/L;
+SENAL_40dB_mp3 = fft(hanning(L).*senal_30dB_mp3,NFFT)/L;
+SENAL_50dB_mp3 = fft(hanning(L).*senal_35dB_mp3,NFFT)/L;
+SENAL_60dB_mp3 = fft(hanning(L).*senal_40dB_mp3,NFFT)/L;
 f = fs/2*linspace(0,1,NFFT/2+1);
 
 % Plot single-sided amplitude spectrum.
@@ -57,31 +46,31 @@ title('Senal 20dB .mp3'), xlabel('Frequency (Hz)'), ylabel('|X(f)|')
 axis([0 150 -200 0])
 
 figure, subplot(2,1,1), plot(f,10*log10(abs(SENAL_30dB(1:NFFT/2+1)))) 
-title('Senal 30dB .wav'), xlabel('Frequency (Hz)'), ylabel('|X(f)|'),
+title('Senal 25dB .wav'), xlabel('Frequency (Hz)'), ylabel('|X(f)|'),
 axis([0 150 -200 0])
 subplot(2,1,2), plot(f,10*log10(abs(SENAL_30dB_mp3(1:NFFT/2+1)))) 
-title('Senal 30dB .mp3'), xlabel('Frequency (Hz)'), ylabel('|X(f)|')
+title('Senal 25dB .mp3'), xlabel('Frequency (Hz)'), ylabel('|X(f)|')
 axis([0 150 -200 0])
 
 figure, subplot(2,1,1), plot(f,10*log10(abs(SENAL_40dB(1:NFFT/2+1)))) 
-title('Senal 40dB .wav'), xlabel('Frequency (Hz)'), ylabel('|X(f)|'),
+title('Senal 30dB .wav'), xlabel('Frequency (Hz)'), ylabel('|X(f)|'),
 axis([0 150 -200 0])
 subplot(2,1,2), plot(f,10*log10(abs(SENAL_40dB_mp3(1:NFFT/2+1)))) 
-title('Senal 40dB .mp3'), xlabel('Frequency (Hz)'), ylabel('|X(f)|')
+title('Senal 30dB .mp3'), xlabel('Frequency (Hz)'), ylabel('|X(f)|')
 axis([0 150 -200 0])
 
 figure, subplot(2,1,1), plot(f,10*log10(abs(SENAL_50dB(1:NFFT/2+1)))) 
-title('Senal 50dB .wav'), xlabel('Frequency (Hz)'), ylabel('|X(f)|'),
+title('Senal 40dB .wav'), xlabel('Frequency (Hz)'), ylabel('|X(f)|'),
 axis([0 150 -200 0])
 subplot(2,1,2), plot(f,10*log10(abs(SENAL_50dB_mp3(1:NFFT/2+1)))) 
-title('Senal 50dB .mp3'), xlabel('Frequency (Hz)'), ylabel('|X(f)|')
+title('Senal 40dB .mp3'), xlabel('Frequency (Hz)'), ylabel('|X(f)|')
 axis([0 150 -200 0])
 
 figure, subplot(2,1,1), plot(f,10*log10(abs(SENAL_60dB(1:NFFT/2+1)))) 
-title('Senal 60dB .wav'), xlabel('Frequency (Hz)'), ylabel('|X(f)|'),
+title('Senal 45dB .wav'), xlabel('Frequency (Hz)'), ylabel('|X(f)|'),
 axis([0 150 -200 0])
 subplot(2,1,2), plot(f,10*log10(abs(SENAL_60dB_mp3(1:NFFT/2+1)))) 
-title('Senal 60dB .mp3'), xlabel('Frequency (Hz)'), ylabel('|X(f)|')
+title('Senal 45dB .mp3'), xlabel('Frequency (Hz)'), ylabel('|X(f)|')
 axis([0 150 -200 0])
 
 
--- syntheticSignalsGeneration.m
+++ syntheticSignalsGeneration.m
@@ -18,35 +18,35 @@ a_enf_20dB=a_sin/100;
 enf_20dB=a_enf_20dB*cos(2*pi()*f_enf*t);
 senal_20dB=enf_20dB+sin;
 
+a_enf_25dB=a_sin/316;
+enf_25dB=a_enf_25dB*cos(2*pi()*f_enf*t);
+senal_25dB=enf_25dB+sin;
+
 a_enf_30dB=a_sin/1000;
 enf_30dB=a_enf_30dB*cos(2*pi()*f_enf*t);
 senal_30dB=enf_30dB+sin;
+
+a_enf_35dB=a_sin/3162;
+enf_35dB=a_enf_35dB*cos(2*pi()*f_enf*t);
+senal_35dB=enf_35dB+sin;
 
 a_enf_40dB=a_sin/10000;
 enf_40dB=a_enf_40dB*cos(2*pi()*f_enf*t);
 senal_40dB=enf_40dB+sin;
 
-a_enf_50dB=a_sin/100000;
-enf_50dB=a_enf_50dB*cos(2*pi()*f_enf*t);
-senal_50dB=enf_50dB+sin;
-
-a_enf_60dB=a_sin/1000000;
-enf_60dB=a_enf_60dB*cos(2*pi()*f_enf*t);
-senal_60dB=enf_60dB+sin;
-
 % Conversion a .wav
 wavwrite(senal_20dB,fs,16,'./dataset/sin/senal_20dB.wav');
+wavwrite(senal_25dB,fs,16,'./dataset/sin/senal_25dB.wav');
 wavwrite(senal_30dB,fs,16,'./dataset/sin/senal_30dB.wav');
+wavwrite(senal_35dB,fs,16,'./dataset/sin/senal_35dB.wav');
 wavwrite(senal_40dB,fs,16,'./dataset/sin/senal_40dB.wav');
-wavwrite(senal_50dB,fs,16,'./dataset/sin/senal_50dB.wav');
-wavwrite(senal_60dB,fs,16,'./dataset/sin/senal_60dB.wav');
 
 % Conversion a .mp3
-mp3write(senal_20dB,fs,16,'./dataset/sin/256_hq/senal_20dB.mp3');
-mp3write(senal_30dB,fs,16,'./dataset/sin/256_hq/senal_30dB.mp3');
-mp3write(senal_40dB,fs,16,'./dataset/sin/256_hq/senal_40dB.mp3');
-mp3write(senal_50dB,fs,16,'./dataset/sin/256_hq/senal_50dB.mp3');
-mp3write(senal_60dB,fs,16,'./dataset/sin/256_hq/senal_60dB.mp3');
+mp3write(senal_20dB,fs,16,'./dataset/sin/senal_20dB.mp3');
+mp3write(senal_25dB,fs,16,'./dataset/sin/senal_25dB.mp3');
+mp3write(senal_30dB,fs,16,'./dataset/sin/senal_30dB.mp3');
+mp3write(senal_35dB,fs,16,'./dataset/sin/senal_35dB.mp3');
+mp3write(senal_40dB,fs,16,'./dataset/sin/senal_40dB.mp3');
 
 %% CHIRP + ENF
 fs=44100; %hz
@@ -63,38 +63,38 @@ a_enf_20dB=a_ch/100;
 enf_20dB=a_enf_20dB*cos(2*pi()*f_enf*t);
 senal_20dB=enf_20dB+ch;
 
+a_enf_25dB=a_ch/316;
+enf_25dB=a_enf_25dB*cos(2*pi()*f_enf*t);
+senal_25dB=enf_25dB+ch;
+
 a_enf_30dB=a_ch/1000;
 enf_30dB=a_enf_30dB*cos(2*pi()*f_enf*t);
 senal_30dB=enf_30dB+ch;
+
+a_enf_35dB=a_ch/3162;
+enf_35dB=a_enf_35dB*cos(2*pi()*f_enf*t);
+senal_35dB=enf_35dB+ch;
 
 a_enf_40dB=a_ch/10000;
 enf_40dB=a_enf_40dB*cos(2*pi()*f_enf*t);
 senal_40dB=enf_40dB+ch;
 
-a_enf_50dB=a_ch/100000;
-enf_50dB=a_enf_50dB*cos(2*pi()*f_enf*t);
-senal_50dB=enf_50dB+ch;
-
-a_enf_60dB=a_ch/1000000;
-enf_60dB=a_enf_60dB*cos(2*pi()*f_enf*t);
-senal_60dB=enf_60dB+ch;
-
 % Conversion a .wav
 wavwrite(senal_20dB,fs,16,'./dataset/chirp/senal_20dB.wav');
+wavwrite(senal_25dB,fs,16,'./dataset/chirp/senal_25dB.wav');
 wavwrite(senal_30dB,fs,16,'./dataset/chirp/senal_30dB.wav');
+wavwrite(senal_35dB,fs,16,'./dataset/chirp/senal_35dB.wav');
 wavwrite(senal_40dB,fs,16,'./dataset/chirp/senal_40dB.wav');
-wavwrite(senal_50dB,fs,16,'./dataset/chirp/senal_50dB.wav');
-wavwrite(senal_60dB,fs,16,'./dataset/chirp/senal_60dB.wav');
 
 % Conversion a .mp3
 mp3write(senal_20dB,fs,16,'./dataset/chirp/senal_20dB.mp3');
+mp3write(senal_35dB,fs,16,'./dataset/chirp/senal_25dB.mp3');
 mp3write(senal_30dB,fs,16,'./dataset/chirp/senal_30dB.mp3');
+mp3write(senal_35dB,fs,16,'./dataset/chirp/senal_35dB.mp3');
 mp3write(senal_40dB,fs,16,'./dataset/chirp/senal_40dB.mp3');
-mp3write(senal_50dB,fs,16,'./dataset/chirp/senal_50dB.mp3');
-mp3write(senal_60dB,fs,16,'./dataset/chirp/senal_60dB.mp3');
 
 %% Voz + ENF
-[voz_original, fs] = wavread('./dataset/voz/voz_pablo_original.wav');
+[voz_original, fs] = wavread('./dataset/voz/voz_pablo_original_2.wav');
 voz_original=voz_original';
 a_voz=max(voz_original);
 l=length(voz_original);
@@ -106,34 +106,34 @@ a_enf_20dB=a_voz/100;
 enf_20dB=a_enf_20dB*cos(2*pi()*f_enf*t);
 senal_20dB=enf_20dB+voz_original;
 
+a_enf_25dB=a_voz/316;
+enf_25dB=a_enf_25dB*cos(2*pi()*f_enf*t);
+senal_25dB=enf_25dB+voz_original;
+
 a_enf_30dB=a_voz/1000;
 enf_30dB=a_enf_30dB*cos(2*pi()*f_enf*t);
 senal_30dB=enf_30dB+voz_original;
+
+a_enf_35dB=a_voz/3162;
+enf_35dB=a_enf_35dB*cos(2*pi()*f_enf*t);
+senal_35dB=enf_35dB+voz_original;
 
 a_enf_40dB=a_voz/10000;
 enf_40dB=a_enf_40dB*cos(2*pi()*f_enf*t);
 senal_40dB=enf_40dB+voz_original;
 
-a_enf_50dB=a_voz/100000;
-enf_50dB=a_enf_50dB*cos(2*pi()*f_enf*t);
-senal_50dB=enf_50dB+voz_original;
-
-a_enf_60dB=a_voz/1000000;
-enf_60dB=a_enf_60dB*cos(2*pi()*f_enf*t);
-senal_60dB=enf_60dB+voz_original;
-
 % Conversion a .wav
 wavwrite(senal_20dB,fs,16,'./dataset/voz/senal_20dB.wav');
+wavwrite(senal_25dB,fs,16,'./dataset/voz/senal_25dB.wav');
 wavwrite(senal_30dB,fs,16,'./dataset/voz/senal_30dB.wav');
+wavwrite(senal_35dB,fs,16,'./dataset/voz/senal_35dB.wav');
 wavwrite(senal_40dB,fs,16,'./dataset/voz/senal_40dB.wav');
-wavwrite(senal_50dB,fs,16,'./dataset/voz/senal_50dB.wav');
-wavwrite(senal_60dB,fs,16,'./dataset/voz/senal_60dB.wav');
 
 % Conversion a .mp3
 mp3write(senal_20dB,fs,16,'./dataset/voz/senal_20dB.mp3');
+mp3write(senal_35dB,fs,16,'./dataset/voz/senal_25dB.mp3');
 mp3write(senal_30dB,fs,16,'./dataset/voz/senal_30dB.mp3');
+mp3write(senal_35dB,fs,16,'./dataset/voz/senal_35dB.mp3');
 mp3write(senal_40dB,fs,16,'./dataset/voz/senal_40dB.mp3');
-mp3write(senal_50dB,fs,16,'./dataset/voz/senal_50dB.mp3');
-mp3write(senal_60dB,fs,16,'./dataset/voz/senal_60dB.mp3');
 
 
